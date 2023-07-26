b16fedd237bdca6abaf563aa00f1cde0757680c7
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-07-26 16:00:16 -0400

    champions -> bushiod

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ fetch(url)
 		for (let i = 0; i < bushido.length; i++) {
 			const img = document.createElement('img');
 			img.classList.add('cardImg');
-			img.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champions[i][0]}_0.jpg`;
+			img.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${bushido[i][0]}_0.jpg`;
 
 			img.id = i;
 			img.addEventListener('click', (e) => {
 				const dojoName = document.querySelector('#dojo-name');
-				dojoName.innerText = champions[e.target.id][0];
+				dojoName.innerText = bushido[e.target.id][0];
 				const dojoTitle = document.querySelector('#dojo-title');
-				dojoTitle.innerText = champions[i][1].title;
+				dojoTitle.innerText = bushido[i][1].title;
 				const dojoWeapon = document.querySelector('#dojo-weapon');
-				dojoWeapon.innerText = champions[i][1].tags.join(`, `);
+				dojoWeapon.innerText = bushido[i][1].tags.join(`, `);
 				const dojoDesc = document.querySelector('#dojo-desc');
-				dojoDesc.innerText = champions[i][1].blurb;
+				dojoDesc.innerText = bushido[i][1].blurb;
 				const dojoSplash = document.querySelector('#dojo-splash');
 				dojoSplash.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champions[i][0]}_0.jpg`;
 				const modal = document.querySelector('.modal-container');
